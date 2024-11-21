3ac62db43f85ad86325ebf20e04dfc5c69fe61a8
timoo <90513578+Tommie1236@users.noreply.github.com>
2024-11-21 15:14:48 +0100

    fix footer not being at bottom if there was too little content in main

--- srv/static/scripts/base.js
+++ srv/static/scripts/base.js
@@ -3,7 +3,8 @@
 
 document.body.addEventListener('scroll', () => {
     // Needed to add the scrolling behavior to the body because of some css in 
-    // the html and body tags that prevent a empty space outside the page when scrolling fast
+    // the html and body tags that prevent a empty space outside the page when
+    // scrolling fast
     // thats also why i use "document.documentElement.scrollTop || document.body.scrollTop"
     // instead of window.scrollY
     const header = document.querySelector('header');
